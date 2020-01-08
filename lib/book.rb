2fa4bc5770b2b7841c53cf2d9f3ee57eb2798f3d
santiago molinari <santomolinari@gmail.com>
2020-01-08 10:55:28 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,20 @@
+class Book
+   
+    attr_accessor :title, :author, :page_count, :genre
+
+    def initialize(title)
+        @title = title
+        @author = author #intance variable
+        @page_count = page_count
+        @genre = genre
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
 
 
+
+end
+
+b1 = Book.new("Catcher in the Rye")
